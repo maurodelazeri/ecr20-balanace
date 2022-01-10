915dd284861c74b93a44de577681902aee7d569e
Mauro Delazeri <maurodelazeri@gmail.com>
2022-01-10 20:19:15 -0300

    wip

---
+++ README.md
@@ -0,0 +1,10 @@
+simple go program to read erc-20 balance of a given account.
+
+to run:
+```
+$ go get
+```
+
+```
+$ go build
+```
---
+++ client.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"math"
+	"math/big"
+	"net/http"
+
+	token "main/token"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type Result struct {
+	UserAddr string
+	Balance  *big.Float
+	Contract string
+	Error    string
+}
+
+func checkBalance(contract string, addr string) Result {
+	result := Result{}
+	client, err := ethclient.Dial("https://empty-divine-sky.matic.quiknode.pro/055c547ecf53b5f1e5fbedf40433c18a72facbed/")
+	if err != nil {
+		result.Error = err.Error()
+		return result
+	}
+
+	tokenAddr := common.HexToAddress(contract)
+	instance, err := token.NewToken(tokenAddr, client)
+	if err != nil {
+		result.Error = err.Error()
+		return result
+	}
+	userAddr := common.HexToAddress(addr)
+
+	bal, err := instance.BalanceOf(&bind.CallOpts{}, userAddr)
+	if err != nil {
+		result.Error = err.Error()
+		return result
+	}
+
+	fbal := new(big.Float)
+	fbal.SetString(bal.String())
+	value := new(big.Float).Quo(fbal, big.NewFloat(math.Pow10(int(18))))
+	result.UserAddr = userAddr.String()
+	result.Balance = value
+	result.Contract = tokenAddr.String()
+	return result
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		contract := c.QueryParam("contract")
+		addr := c.QueryParam("addr")
+		if len(contract) == 0 || len(addr) == 0 {
+			contract = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"
+			addr = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
+		}
+
+		result := checkBalance(contract, addr)
+
+		data, err := json.Marshal(result)
+		if err != nil {
+			logrus.Errorln(err)
+			return c.String(http.StatusBadGateway, "")
+		}
+		return c.String(http.StatusOK, string(data))
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
